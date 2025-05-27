8b563d166f823de430fa6aa35c24297841f7e5ed
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-05-28 00:17:41 +0200

    add js files

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { readContacts } from "../utils/readContacts";
+import { writeContacts } from "../utils/writeContacts";
+import { createFakeContact } from '../utils/createFakeContact';
+
+const generateContacts = async (number) => {
+    try {
+        const existingContacts = await readContacts();
+        const newContacts = Array.from({ length: number }, () => createFakeContact());
+        const allContacts = [...existingContacts, ...newContacts];
+        await writeContacts(allContacts);
+        console.log(`Успішно згенеровано та додано ${number} контактів.`);
+    } catch (err) {
+        console.error(
+            'Помилка під час генерації контакту:', err);
+
+    }
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    const data = JSON.stringify(updatedContacts, null, 2);
+    
+    try {
+         await fs.writeFile(PATH_DB, data, 'utf8');
+        console.log('Контакти успішно записані');
+    } catch (err) {
+        console.error('Помилка запису у файл:', err);
+    }
+};
